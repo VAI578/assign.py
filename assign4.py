dd6732d8e9b29fc9c8a6ce199f11b7b7692c669b
VAISHNAVI SUNIL PURIGOSAVI <gosavivaishnavi9370@gmail.com>
2026-04-19 17:20:18 +0530

    Update assign4.py

--- assign4.py
+++ assign4.py
@@ -1,1 +1,21 @@
 
+# Task 1: Read a File and Handle Errors
+#
+try:
+    
+    file = open("sample.txt", "r")
+
+    print("File content:\n")
+
+  
+    for line in file:
+        print(line.strip())
+        
+        
+    file.close()
+
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
+
+except Exception as e:
+    print("An unexpected error occurred:", e)
